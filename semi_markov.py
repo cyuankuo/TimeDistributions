432c1c070edbf6472601daf2e839b758f69e6962
akalenkova <46219277+akalenkova@users.noreply.github.com>
2022-11-24 09:45:54 +1030

    Remove processed transtions

--- semi_markov.py
+++ semi_markov.py
@@ -25,10 +25,14 @@ class SemiMarkov:
             #  Add new transitions
             in_transitions = self.get_in_transitions(state)
             out_transitions = self.get_out_transitions(state)
+            number_of_new_transitions = len(in_transitions) * len(out_transitions)
+            i = 1
             for in_transition in in_transitions:
                 in_state = in_transition[0]
                 for out_transition in out_transitions:
                     out_state = out_transition[1]
+                    print("Adding transiton " + str(i) + " out of " + str(number_of_new_transitions))
+                    i += 1
                     p = self.get_probability(in_state, out_state)
                     time = self.get_time(in_state, out_state)
                     new_p = self.get_probability(in_state,state)*self.get_probability(state,out_state)/(1-self.get_probability(state,state))
@@ -39,12 +43,21 @@ class SemiMarkov:
                     new_time = mult_gauss_convolution(new_time, m2)
                     all_time = mult_gauss_sum(time, new_time, p/(p+new_p), new_p/(p+new_p))
 
+                    # Remove old transition
+                    transition_to_remove = set()
+                    for transition in self.transitions:
+                        if ((transition[0] == in_state) and (transition[1] == out_state)):
+                            transition_to_remove.add(transition)
+                    for transition in transition_to_remove:
+                        self.transitions.remove(transition) 
+                    
                     # Add new transition
                     self.transitions.add((in_state, out_state, all_p, all_time))
-                    all_time.plot_mult_gauss(range(0,1000,1))
-                    print(all_time.probabilities)
-                    plt.title(label=label)
-                    plt.show()
+
+                    #all_time.plot_mult_gauss(range(0,1000,1))
+                    #print(all_time.probabilities)
+                    #plt.title(label=label)
+                    #plt.show()
 
             # Remove state
             transition_to_remove = set()
